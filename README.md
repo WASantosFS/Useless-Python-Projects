5cee071ef8ee92e6fa6c22f782ae60c3425664da
W. A. Santos <baekgu@live.com>
2020-12-30 15:13:39 -0500

    Project 1, completed: Silly Name Generator.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-Useless-Python-Projects
+# Useless Python Projects
+## Scope
+The purpose of this is to reinforce what I have been learning on PluralSight's "Core Python" path. I noticed when I has over 60% of the way through that I was essentially learning on an academic level, without homework or doing projects. This is an effort to correct this.
+
+These projects are from the "Impractical Python Projects" book. The goal, ultimately, is to go through the entire book, but also to complete additional 'side' projects that are off-shoots that may interest me.
+
+## Index
+1) Silly Name Generator
+2) Finding Palingram Spells
+3) Solving Anagrams
+4) Decoding American Civil War Ciphers
+5) Encoding English Civil War Ciphers
+6) Writing in Invisible Ink
+7) Breeding Giant Rats with Genetic Algorithms
+8) Counting Syllables for Haiku Poetry
+9) Writing Haiku with Markov Chain Analysis
+10) Are we Alone? Exploring the Fermi Paradox
+11) The Monty Hall Problem
+12) Securing Your Nest Egg
+13) Simulating an Alien Volcano
+14) Mapping Mars with the Mars Orbiter
+15) Improving Your Astrophotography with Planet Stacking
+16) Finding Frauds with Benford's Law
---
+++ silly_name_generator/pseudonyms.py
@@ -0,0 +1,76 @@
+"""
+Project 1: Silly Name Generator.
+
+Objective: To make a name generator using code that conforms with established style guidelines.
+
+Pseudocode:
+    Load a list of first names
+    Load a list of surnames
+    Choose a first name at random
+    Assign the name to a variable
+    Choose a surname at random
+    Assign the name to a variable
+    4 Chapter 1
+    Print the names to the screen in order and in red font
+    Ask the user to quit or play again
+    If user plays again:
+     repeat
+    If user quits:
+     end and exit
+"""
+import sys
+import random
+
+
+def main():
+    """Choose names at random from 2 tuples and print to screen."""
+    print("Welcome to the Silly Name Generator!\n")
+
+    # Tuples taken from "Impractical Python Projects" book.
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+             "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ",
+             'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+             'Chewy', 'Chigger", "Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+             'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+             'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+             'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+             'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"',
+             'Mergatroid', '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch',
+             'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug',
+             'Pushmeet', 'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+             "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+             'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+             'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+             "Winston 'Jazz Hands'", 'Worms')
+
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+            'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+            'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+            'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+            'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+            'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+            'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+            'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+            'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+            'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+            'Woolysocks')
+
+    while True:
+        first_name = random.choice(first)
+
+        last_name = random.choice(last)
+
+        print(f"{first_name} {last_name}", file=sys.stderr)
+
+        try_again = input("Try again? (Press Enter, else q to quit.)\n")
+        if try_again.lower() == "q":
+            break
+
+    input("\nPress Enter to exit.")
+
+
+if __name__ == "__main__":
+    main()
